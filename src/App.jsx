6c2cc2eb63362646442bc23f6df5875ed0525a1f
Rishita Raj <rishitaraj1855@gmail.com>
2025-10-28 13:05:31 +0530

    contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,3 @@
-// import React from 'react'
-// import { useEffect  } from 'react';
-// import {gsap} from 'gsap';
-// import {ScrollTrigger} from 'gsap/ScrollTrigger';
-
-// import { Routes, Route } from 'react-router-dom';
-// import Profiles from './components/Profiles.jsx';
-// import Header from './components/Header.jsx';
-// import HeroSection from './components/HeroSection.jsx';
-// import CustomCursor from './components/CustomCursor.jsx';
-// import AboutSection from './components/AboutSection.jsx';
-// import ProjectSection from './components/ProjectSection.jsx';
-// import TechnologiesSection from './components/TechnologiesSection.jsx';
-// import Contact from './components/contact.jsx';
-// import Footer from './components/Footer.jsx';
-
-// const App = () => {
-
-// useEffect(() => {
-//   // register scrolltrigger plugin
-//   gsap.registerPlugin(ScrollTrigger);
-
-//   //refresh scroll trigger when the page is fully loaded
-//   ScrollTrigger.refresh()
-
-//   //clean
-//   return () =>{
-//     ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-//   }
-// } ,[])
-
-//   return (
-//     <>
-//   <Header/>
-//   <HeroSection/>
-//   <CustomCursor/>
-//   <AboutSection/>
-//   <ProjectSection/>
-//   <Profiles/>
-//   <TechnologiesSection/>
-//  <Contact />
-//   <Footer/>
-//   {/* <Routes>
-//     <Route path="/profiles" element={<Profiles />} />
-//   </Routes> */}
-//     </>
-//   )
-// }
-
-// export default App
-
-
 import { useEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -61,7 +9,7 @@ import CustomCursor from "./components/CustomCursor.jsx";
 import AboutSection from "./components/AboutSection.jsx";
 import ProjectSection from "./components/ProjectSection.jsx";
 import TechnologiesSection from "./components/TechnologiesSection.jsx";
-import Contact from "./components/contact.jsx";
+import Contact from "./components/Contact.jsx";
 import Footer from "./components/Footer.jsx";
 
 const App = () => {
